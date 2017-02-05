ea48a39f72aa890e32705b8f4af94978897ae20f
Joshua Massover <joshua.massover@gmail.com>
2017-02-04 20:44:39 -0500

    Add travis

---
+++ killington/settings/test.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = False
+SECRET_KEY = 'ia!%$GLg!gsDf8$^cq$SR+ZUkglYUmVNLYPeZb-lxjM_V4#7Rw'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'killington_test',
+        'USER': 'killington',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
