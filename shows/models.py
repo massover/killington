4ac2f01eb5da0494d26d8aa87ae4a11590bf55aa
Joshua Massover <joshua.massover@gmail.com>
2017-02-04 23:41:32 -0500

    Update model __str__

--- shows/models.py
+++ shows/models.py
@@ -1,11 +1,15 @@
 from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.formats import date_format
 from django.utils.translation import ugettext_lazy as _
 
 
 class Show(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class ShowTime(models.Model):
@@ -14,6 +18,10 @@ class ShowTime(models.Model):
 
     class Meta:
         verbose_name = 'Show Time'
+
+    def __str__(self):
+        starts_at = date_format(self.starts_at, 'DATETIME_FORMAT')
+        return '%s %s' % (self.show.name, starts_at)
 
 
 class Lottery(models.Model):
@@ -24,6 +32,10 @@ class Lottery(models.Model):
 
     class Meta:
         verbose_name_plural = 'Lotteries'
+
+    def __str__(self):
+        starts_at = date_format(self.starts_at, 'DATETIME_FORMAT')
+        return '%s Lottery %s' % (self.show.name, starts_at)
 
 
 numeric_validator = RegexValidator(r'^[0-9]*$', _('Only numbers allowed.'))
