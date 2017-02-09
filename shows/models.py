bcd4fcc8b3b54cf2d44a7e9f601a06c8c2b1f635
Joshua Massover <joshua.massover@gmail.com>
2017-02-09 13:45:34 -0500

    Update models

--- shows/models.py
+++ shows/models.py
@@ -4,44 +4,60 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.formats import date_format
 from django.utils.translation import ugettext_lazy as _
 
+from .managers import ActiveLotteryManager
+
 
 class Show(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=31)
+    subscribed_users = models.ManyToManyField('User')
 
     def __str__(self):
-        return self.name
+        return 'Show: %s' % self.name
 
 
-class ShowTime(models.Model):
+class Performance(models.Model):
     show = models.ForeignKey(Show)
     starts_at = models.DateTimeField()
-
-    class Meta:
-        verbose_name = 'Show Time'
 
     def __str__(self):
         starts_at = date_format(self.starts_at, 'DATETIME_FORMAT')
-        return '%s %s' % (self.show.name, starts_at)
+        return 'Performance: %s %s' % (self.show.name, starts_at)
 
 
 class Lottery(models.Model):
-    show = models.ForeignKey(Show)
+    objects = models.Manager()
+    active_objects = ActiveLotteryManager()
+
+    performance = models.OneToOneField(Performance)
     lottery_id = models.IntegerField()
     nonce = models.CharField(max_length=31)
     starts_at = models.DateTimeField()
+    processed = models.BooleanField(default=False)
+    entered_users = models.ManyToManyField('User')
 
     class Meta:
         verbose_name_plural = 'Lotteries'
 
+    @property
+    def url(self):
+        return 'https://lottery.broadwaydirect.com/enter-lottery/?lottery={}&window=popup'.format(
+            self.lottery_id
+        )
+
+    @property
+    def http_referer(self):
+        return '/enter-lottery/?lottery={}&window=popup'.format(self.lottery_id)
+
     def __str__(self):
         starts_at = date_format(self.starts_at, 'DATETIME_FORMAT')
-        return '%s Lottery %s' % (self.show.name, starts_at)
+        return 'Lottery: %s %s' % (self.performance.show.name, starts_at)
 
 
 numeric_validator = RegexValidator(r'^[0-9]*$', _('Only numbers allowed.'))
 
+
 class User(AbstractUser):
-    date_of_birth = models.DateTimeField(_('date of birth'))
+    date_of_birth = models.DateField(_('date of birth'))
     zipcode = models.CharField(_('ZIP code'), max_length=5, validators=[numeric_validator])
 
     REQUIRED_FIELDS = ['email', 'date_of_birth', 'zipcode']
