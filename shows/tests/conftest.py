bf9a640b64f1f9de8e94016be2f68fc6c263fb3b
Joshua Massover <joshua.massover@gmail.com>
2017-02-09 13:46:46 -0500

    Add pytest fixtures

---
+++ shows/tests/conftest.py
@@ -0,0 +1,19 @@
+from django.utils import timezone
+from datetime import timedelta
+
+import pytest
+from pytest_factoryboy import register, LazyFixture
+
+from ..factories import (LotteryFactory, ShowFactory, PerformanceFactory,
+                         UserFactory)
+
+register(UserFactory)
+register(ShowFactory)
+register(PerformanceFactory)
+register(LotteryFactory, 'active_lottery', processed=False,
+         starts_at=timezone.now() - timedelta(minutes=30))
+
+
+@pytest.fixture
+def show__subscribed_users(user):
+    return [user]
