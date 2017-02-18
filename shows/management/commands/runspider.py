836c0eb26b12c3827b7ec4ff3e785ceab23a8afa
Joshua Massover <joshua.massover@gmail.com>
2017-02-18 15:56:16 -0500

    Add command to runspider through django

---
+++ shows/management/commands/runspider.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from scrapy.crawler import CrawlerProcess
+
+from ...spiders import ShowsSpider
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        process = CrawlerProcess({
+            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+        })
+        process.crawl(ShowsSpider)
+        process.start()
