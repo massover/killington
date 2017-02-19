4c8887e519408b4acc1b85c32e7d3a9218ebfb58
Joshua Massover <joshua.massover@gmail.com>
2017-02-18 19:19:12 -0500

    Add timezone middleware

---
+++ killington/middlewares.py
@@ -0,0 +1,8 @@
+import pytz
+
+from django.utils import timezone
+from django.utils.deprecation import MiddlewareMixin
+
+class TimezoneMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        timezone.activate(pytz.timezone('US/Eastern'))
