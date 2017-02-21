7f7a7bf4dcda94524a2c8faac45cb2216981054f
Joshua Massover <joshua.massover@gmail.com>
2017-02-20 21:32:25 -0500

    Add CELERYD_MAX_TASKS_PER_CHILD setting

--- killington/settings/base.py
+++ killington/settings/base.py
@@ -119,4 +119,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 CELERY_RESULT_BACKEND = 'django-db'
 
 # http://docs.celeryproject.org/en/latest/userguide/configuration.html?highlight=SOFT_TIME_LIMIT#std:setting-task_time_limit
-CELERYD_CELERYD_TASK_TIME_LIMIT = 60
+CELERY_CELERYD_TASK_TIME_LIMIT = 60
+
+# http://stackoverflow.com/questions/11528739/running-scrapy-spiders-in-a-celery-task/11657239#11657239
+CELERY_CELERYD_MAX_TASKS_PER_CHILD = 1
