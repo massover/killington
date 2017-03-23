e1053abce4bdb5cf3ba400d31a5840e53cb92b0e
Josh Massover <pair+joshua@voxy.com>
2017-03-22 22:26:26 -0400

    Try html in confirmation email

--- shows/mixins.py
+++ shows/mixins.py
@@ -1,3 +1,4 @@
+from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import redirect
 from django.urls import reverse
 
@@ -21,8 +22,13 @@ class SendConfirmationEmailMixin(object):
         kwargs = {'confirmation_code': self.request.user.confirmation_code}
         uri = reverse('email-confirmation-code-validation', kwargs=kwargs)
         link = self.request.build_absolute_uri(uri)
-        self.request.user.email_user(
-            subject='Confirm registration for nycbroadwayclub.com',
-            message='Verify your account using this link: {}'.format(link),
-            from_email='noreply@nycbroadwayclub.com',
-        )
+
+        subject = 'Confirm registration for nycbroadwayclub.com'
+        text_content = 'Almost there!'
+        from_email = 'noreply@nycbroadwayclub.com'
+        to = [self.request.user.email]
+        msg = EmailMultiAlternatives(subject, text_content, from_email, to)
+
+        html_content = '<p><a href="{}">Click here<a> to confirm your email</p>'.format(link)
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
