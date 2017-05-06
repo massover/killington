943847899c6104077d899cda95ad24c9bd9f5da4
Josh Massover <pair+joshua@voxy.com>
2017-05-06 18:09:57 -0400

    Update celery beat schedule
    - Close #23

--- killington/celery.py
+++ killington/celery.py
@@ -25,23 +25,29 @@ app.conf.task_routes = {
     'shows.tasks.enter_user_in_lottery_for_flood': {'queue': 'flood'}
 }
 
+# shows.tasks.run_shows_spider needs to scrape the data off the site once a
+# lottery is open to set lottery objects as enterable_objects. run_shows_spider
+# finishes on the order of seconds, eg.
+# Task shows.tasks.run_shows_spider[40faa8ce-796d-4ac0-98b8-53b8310f8282] succeeded in 2.1336671939934604s: None
+#
+# The schedule is set up to run the lottery scraping on the first minute of
+# every hour, then process enterable loterries on the fifth minute.
 app.conf.CELERYBEAT_SCHEDULE = {
-    'process-enterable-lotteries-every-hour': {
-        'task': 'shows.tasks.process_enterable_lotteries',
-        'schedule': crontab(minute=45, hour='*'),
-        'args': None,
-        'options': {'queue': 'celery'},
-    },
     'run-shows-spider-every-hour': {
         'task': 'shows.tasks.run_shows_spider',
-        'schedule': crontab(minute=15, hour='*'),
+        'schedule': crontab(minute=1, hour='*'),
         'args': None,
         'options': {'queue': 'celery'},
     },
-
+    'process-enterable-lotteries-every-hour': {
+        'task': 'shows.tasks.process_enterable_lotteries',
+        'schedule': crontab(minute=5, hour='*'),
+        'args': None,
+        'options': {'queue': 'celery'},
+    },
     'process-enterable-floods-every-hour': {
         'task': 'shows.tasks.process_enterable_floods',
-        'schedule': crontab(minute=1, hour='*'),
+        'schedule': crontab(minute=5, hour='*'),
         'args': None,
         'options': {'queue': 'flood'},
     },
